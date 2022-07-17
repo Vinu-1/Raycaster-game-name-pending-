99c1f6ff127f25acc4006034e74ac1f23b314c63
Vinu-1 <109447363+Vinu-1@users.noreply.github.com>
2022-07-17 19:44:39 +0530

    Update raycaster.cpp

--- raycaster.cpp
+++ raycaster.cpp
@@ -4,9 +4,21 @@
 
 //Currently has a window with opengl working after pain staking effort and eventually giving up
 
+float px,py; //Player Position
+
+void drawPlayer()
+{
+	glColor3f(1,1,0);
+	glPointSize(8);
+	glBegin(GL_POINTS);
+	glVertex2i(px,py);
+	glEnd();
+}
+
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	drawPlayer();
 	glutSwapBuffers();
 	
 }
@@ -15,6 +27,7 @@ void init()
 {
 	glClearColor(0.3, 0.3, 0.3, 0);
 	gluOrtho2D(0,1024,512,0);
+	px=300; py=300;
 }
 
 int main(int argc, char* argv[]) 
